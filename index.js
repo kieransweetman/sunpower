35613ac9fa25205ddc106ba38192531b2ac7c9b5
Anthony <desmartin.anthony@gmail.com>
2022-10-27 14:44:59 +0200

    Contact Enterprise Form

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 const burgerMenuIcon = document.getElementById('burgerMenuIcon');
 
 burgerMenuIcon.addEventListener('click', (event) => {
-	const burgerExpanded = document.getElementById('burgerExpanded');
-	const mainContent = document.getElementById('main');
-	burgerExpanded.classList.toggle('d-none');
-	mainContent.classList.toggle('d-none');
+  const burgerExpanded = document.getElementById('burgerExpanded');
+  const mainContent = document.getElementById('main');
+  burgerExpanded.classList.toggle('d-none');
+  mainContent.classList.toggle('d-none');
 });
